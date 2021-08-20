d12d52ed10d4692f605235f614c6af9f53fd2f92
Camilo Espinosa <espinosacamilop1@gmail.com>
2021-08-20 00:54:44 -0400

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,40 @@
 const express = require('express');
+const app = require('../app');
 const router = express.Router();
+const Movie = require("../models/Movie.model")
+
+router.use(express.static('public'))
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => {
+    res.render('index', {style: "stylesheets/home.css"})
+    
+    
+    });
+
+router.get("/movies", (req, res, next)=> 
+{
+    Movie.find()
+    .then(allMoviesFromDB => {
+        res.render("movies-list" ,  
+        {
+            movies: allMoviesFromDB,
+            style: "stylesheets/details.css"
+        })
+    })
+    .catch(error => console.log("An error occurred while getting books from database: ", error))
+});
+
+router.get("/movies/:movieId", (req, res, next) =>
+{
+    Movie.findById(req.params.movieId)
+    .then(movieFromDB =>{
+        res.render("movies-detail", 
+         {  movie: movieFromDB,
+            style: "../stylesheets/list.css"
+        })
+    })
+    .catch(error => console.log("An error occurred while getting books from database: ", error))
+})
 
 module.exports = router;
